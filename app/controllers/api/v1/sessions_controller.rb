fd65be2f8a2df7a605a2018a50edf0b57bb58429
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2020-07-04 18:30:55 -0300

    Better conditionals on SessionsController, make sure only 1 path runs

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,15 +5,22 @@ class Api::V1::SessionsController < Api::V1::BaseController
 
   def validate_launch
     app_launch = AppLaunch.find_by(nonce: params[:token])
-    render(json: { token: params[:token], valid: false }.to_json) unless app_launch
-    render(json: { token: params[:token], valid: true, message: JSON.parse(app_launch.message) }.to_json)
+    if app_launch.present?
+      msg = { token: params[:token], valid: true, message: JSON.parse(app_launch.message) }
+      render(json: msg.to_json)
+    else
+      render(json: { token: params[:token], valid: false }.to_json)
+    end
   end
 
   def invalidate_launch
     app_launch = AppLaunch.find_by(nonce: params[:token])
-    render(json: { token: params[:token], changed: false }.to_json) unless app_launch
-
-    app_launch.invalidate!
-    render(json: { token: params[:token], changed: true }.to_json)
+    if app_launch.present?
+      app_launch.invalidate!
+      changed = true
+    else
+      changed = false
+    end
+    render(json: { token: params[:token], changed: changed }.to_json)
   end
 end
