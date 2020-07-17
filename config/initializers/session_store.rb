69929b21e2d55b064f38b3c65bc84847d2f2b797
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2020-07-16 21:29:49 -0300

    Configure the session cookie with SameSite=None and Secure
    Chrome is blocking embedded pages that don't have these properties set on
    the cookies.
    
    The full message is:
    A cookie associated with a cross-site resource at ___ was set without
    the `SameSite` attribute. A future release of Chrome will only deliver
    cookies with cross-site requests if they are set with `SameSite=None`
    and `Secure`. You can review cookies in developer tools under
    Application>Storage>Cookies and see more details at
    https://www.chromestatus.com/feature/5088147346030592 and
    https://www.chromestatus.com/feature/5633521622188032.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,9 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store(:cookie_store, key: '_bbb_lti_broker_session')
+Rails.application.config.session_store(
+  :cookie_store,
+  key: '_bbb_lti_broker_session',
+  same_site: :none,
+  secure: Rails.env.production?
+)
