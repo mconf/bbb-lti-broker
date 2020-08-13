55cde7cfc37b17b7d9c5bcdf9875b8072a614b8f
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2020-08-13 16:26:59 -0300

    Serve assets in production, use env vars to configure it
    - Serve assets if RAILS_SERVE_STATIC_FILES is set
    - Set cache-control in the asset files
    - Configure a CDN using ASSET_HOST
    - Remove empty css files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,10 +16,6 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
@@ -27,10 +23,23 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
+  if ENV['RAILS_SERVE_STATIC_FILES'].present?
+    # Disable serving static files from the `/public` folder by default since
+    # Apache or NGINX already handles this.
+    config.public_file_server.enabled = true
+
+    # Set a cache-control for all assets
+    config.public_file_server.headers = {
+      'Cache-Control' => 'public, s-maxage=31536000, max-age=31536000'
+    }
+  end
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
+
+  unless ENV['ASSET_HOST'].blank?
+    config.action_controller.asset_host = ENV['ASSET_HOST']
+  end
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
