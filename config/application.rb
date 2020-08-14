8939d07993ce5b3e6d15ae302dc791fc35ba12de
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2020-08-13 21:06:05 -0300

    Move the version to config/application and use it if BUILD_NUMBER is nil

--- config/application.rb
+++ config/application.rb
@@ -12,12 +12,14 @@ Bundler.require(*Rails.groups)
 
 module BbbLtiBroker
   class Application < Rails::Application
+    VERSION = "0.0.8-elos"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.url_host = ENV['URL_HOST']
 
-    config.build_number = ENV['BUILD_NUMBER'] || 'v1'
+    config.build_number = ENV['BUILD_NUMBER'] || VERSION
 
     config.log_level = ENV['LOG_LEVEL'] || :debug
 
--- lib/version.rb
+++
@@ -1,5 +0,0 @@
-module BbbLtiBroker
-  class Application
-    VERSION = "0.0.8-elos"
-  end
-end
