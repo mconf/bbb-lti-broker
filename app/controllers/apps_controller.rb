8ef6132dbda98d65535b9895c33560d2a5be3fa6
Christopher POttes <christopherpottesc@gmail.com>
2021-11-30 14:15:31 -0300

    feature(jobs): create jobs for remove launches old and configure scheduler

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -22,16 +22,6 @@ class AppsController < ApplicationController
     message = lti_launch.message
     message.custom_params['oauth_consumer_key'] = params[:oauth_consumer_key]
 
-    # FIX ME
-    # Move to a worker or cache the result
-    date_limit = Rails.configuration.launch_days_to_delete.days.ago.utc
-    query_started = Time.now.utc
-    deleted_launches = AppLaunch.where('expiration_time < ?', date_limit).delete_all
-    query_duration = Time.now.utc - query_started
-    Rails.logger.info "Removing the old AppLaunches from before #{date_limit}, " \
-                      "#{deleted_launches} AppLaunches deleted, " \
-                      "in: #{query_duration.round(3)} seconds"
-
     AppLaunch.find_or_create_by(nonce: lti_launch.nonce) do |launch|
       launch.update(tool_id: tool.id, message: message.to_json)
     end
