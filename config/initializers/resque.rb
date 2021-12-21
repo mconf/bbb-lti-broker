20be8ef30640787b7dc3b6deb40b55c2934bdc80
wpramio <willianpramio@hotmail.com>
2021-12-21 16:53:04 -0300

    fix: use the Rails log_formatter and log_level for Resque logger
    Also fix the indentation

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,29 +3,26 @@ require 'resque/server'
 require 'resque/scheduler/server'
 require 'active_scheduler'
 
-  logger = Logger.new(STDOUT)
-  logger.formatter = proc do |severity, datetime, progname, msg|
-    formatted_datetime = datetime.strftime("%Y-%m-%d %H:%M:%S.") << datetime.usec.to_s[0..2].rjust(3)
-    "#{formatted_datetime} [#{severity}] #{msg} (pid:#{$$})\n"
-  end
-  Resque.logger = logger
-  Resque.logger.level = Logger::INFO
+logger = Logger.new(STDOUT)
+logger.formatter = Rails.application.config.log_formatter
+logger.level = Rails.application.config.log_level
+Resque.logger = logger
 
-  attrs = {
-    host: Rails.application.config.redis_host,
-    port: Rails.application.config.redis_port,
-    db: Rails.application.config.redis_db
-  }
-  attrs[:password] = Rails.application.config.redis_password unless Rails.application.config.redis_password.blank?
-  Resque.redis = Redis.new(attrs)
+attrs = {
+  host: Rails.application.config.redis_host,
+  port: Rails.application.config.redis_port,
+  db: Rails.application.config.redis_db
+}
+attrs[:password] = Rails.application.config.redis_password unless Rails.application.config.redis_password.blank?
+Resque.redis = Redis.new(attrs)
 
-  # Scheduler configuration
-  yml_schedule    = YAML.load_file("config/jobs_schedule.yml")
-  wrapped_schedule = ActiveScheduler::ResqueWrapper.wrap yml_schedule
-  Resque.schedule  = wrapped_schedule
+# Scheduler configuration
+yml_schedule     = YAML.load_file("config/jobs_schedule.yml")
+wrapped_schedule = ActiveScheduler::ResqueWrapper.wrap yml_schedule
+Resque.schedule  = wrapped_schedule
 
-  # Authentication for the Resque web interface
-  Resque::Server.use(Rack::Auth::Basic) do |user, password|
-    user == ENV["ADMIN_KEY"]
-    password == ENV["ADMIN_PASSWORD"]
-  end
+# Authentication for the Resque web interface
+Resque::Server.use(Rack::Auth::Basic) do |user, password|
+  user == ENV["ADMIN_KEY"]
+  password == ENV["ADMIN_PASSWORD"]
+end
