b364f9af374840c5e0953147ea8df7c225d3339b
matheus lagreca <matheus.lagreca@gmail.com>
2024-01-17 17:24:17 -0300

    fix(rails_admin): removed relative path config

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -28,7 +28,3 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-
-unless ENV['RELATIVE_URL_ROOT'].blank?
-  Rails.application.config.assets.prefix = "/#{ENV['RELATIVE_URL_ROOT']}/assets"
-end
