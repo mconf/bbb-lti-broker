6b691a86fc4b1104bfc27ac2f13299ec00557b41
matheus lagreca <matheus.lagreca@gmail.com>
2024-01-18 10:21:22 -0300

    fix(rubocop): removed unused initalized method
    super is used by default

--- lib/clients/coc/api/data/container_data.rb
+++ lib/clients/coc/api/data/container_data.rb
@@ -4,10 +4,6 @@ module Clients::Coc
   module Api
     module Data
       class ContainerData < Array
-        def initialize
-          super
-        end
-
         def find_by(element_id)
           find { |e| e.id == element_id }
         end
