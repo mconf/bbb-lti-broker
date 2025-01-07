5e861e9b9d1451b42c17f80e49e8278c88150a9b
Willian Pramio <willianpramio@hotmail.com>
2025-01-07 18:46:34 -0300

    feat(Tool): methods to set and remove app settings

--- config/initializers/rails_lti2_provider.rb
+++ config/initializers/rails_lti2_provider.rb
@@ -1,14 +1,6 @@
 Rails.application.config.to_prepare do
 
   RailsLti2Provider::Tenant.class_eval do
-    def get_app_settings(app_name)
-      if app_name.blank? || Doorkeeper::Application.find_by(name: app_name).nil?
-        raise(ArgumentError, 'Invalid app name')
-      end
-
-      self.settings[app_name]
-    end
-
     def set_app_settings(key_or_hash, value = nil, app_name:)
       if app_name.blank? || Doorkeeper::Application.find_by(name: app_name).nil?
         raise(ArgumentError, 'Invalid app name')
@@ -33,6 +25,36 @@ Rails.application.config.to_prepare do
       raise(ArgumentError, 'At least 1 key must be informed') if keys.blank?
 
       self.settings[app_name] = self.settings[app_name].except(*keys)
+
+      save
+    end
+  end
+
+  RailsLti2Provider::Tool.class_eval do
+    def set_app_settings(key_or_hash, value = nil, app_name:)
+      if app_name.blank? || Doorkeeper::Application.find_by(name: app_name).nil?
+        raise(ArgumentError, 'Invalid app name')
+      end
+      raise(ArgumentError, 'Key cannot be nil') if key_or_hash.nil?
+
+      new_settings = self.app_settings[app_name] || {}
+      if key_or_hash.is_a?(Hash)
+        new_settings = new_settings.merge(key_or_hash)
+      else
+        new_settings[key_or_hash] = value
+      end
+      self.app_settings[app_name] = new_settings
+
+      save
+    end
+
+    def remove_app_settings(*keys, app_name:)
+      if app_name.blank? || Doorkeeper::Application.find_by(name: app_name).nil?
+        raise(ArgumentError, 'Invalid app name')
+      end
+      raise(ArgumentError, 'At least 1 key must be informed') if keys.blank?
+
+      self.app_settings[app_name] = self.app_settings[app_name].except(*keys)
 
       save
     end
