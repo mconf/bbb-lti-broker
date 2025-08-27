ca39d2ddf80fe36bb6db193f309dec1b54e56b0c
Willian Pramio <willianpramio@hotmail.com>
2025-08-27 14:01:36 -0300

    feat(app_configs): configure RailsAdmin
    to replace the own-made dashboard (`lti/registration/list`)

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,30 +1,12 @@
 # frozen_string_literal: true
 
 RailsAdmin.config do |config|
-  ### Popular gems integration
-
-  ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
-
-  ## == CancanCan ==
-  # config.authorize_with :cancancan
-
-  ## == Pundit ==
-  # config.authorize_with :pundit
-
-  ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
-
-  ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
-
-  ## == Gravatar integration ==
-  ## To disable Gravatar integration in Navigation Bar set to false
-  # config.show_gravatar = true
+  config.main_app_name = 'Broker'
 
   config.asset_source = :sprockets
+
+  # To avoid CSRF errors
+  config.forgery_protection_settings = { with: :null_session }
 
   # If true, you must authenticate to use rails admin
   if Mconf::Env.fetch_boolean('AUTHENTICATION_RAILS_ADMIN', true)
@@ -35,6 +17,7 @@ RailsAdmin.config do |config|
     end
   end
 
+  ###### Actions configs
   config.actions do
     dashboard                     # mandatory
     index                         # mandatory
@@ -44,12 +27,235 @@ RailsAdmin.config do |config|
     show
     edit
     delete
-    show_in_app
-
-    ## With an audit adapter, you can add:
-    # history_index
-    # history_show
   end
 
-  config.included_models = ['RailsLti2Provider::Tool', 'RailsLti2Provider::Tenant'] if Rails.env.production?
+  config.excluded_models = ['RailsLti2Provider::Registration']
+
+  ###### Model configs
+  ### Doorkeeper::Application ###
+  config.model Doorkeeper::Application do
+    configure :confidential do
+      help 'Application will be used where the client secret can be kept confidential. ' \
+      'Native mobile apps and Single Page Apps are considered non-confidential.'
+    end
+    configure :scopes do
+      help 'Separate scopes with spaces. Leave blank to use the default scopes.'
+    end
+    configure :authorized_applications do
+      hide
+    end
+  end
+  ### Doorkeeper::Application ###
+
+
+  ### BbbConfig ###
+  config.model BbbConfig do
+    label 'BBB Config'
+
+    list do
+      configure [:created_at, :updated_at] do
+        hide
+      end
+    end
+
+    edit do
+      configure :tool do
+        hide
+      end
+    end
+  end
+  ### BbbConfig ###
+
+
+  ### RoomsAppConfig ###
+  config.model RoomsAppConfig do
+    list do
+      configure [:created_at, :updated_at] do
+        hide
+      end
+    end
+
+    show do
+      configure [:created_at, :updated_at]
+    end
+
+    edit do
+      configure :tool do
+        hide
+      end
+
+      include_all_fields
+      group :default do
+      end
+
+      # Moodle Configs
+      group :moodle_configs do
+        # help 'When not configured, the MoodleToken is destroyed in Rooms database.'
+        active false
+
+        field :moodle_integration_enabled do
+          label 'Moodle integration enabled'
+          help 'If disabled, the Moodle configs are not sent. The MoodleToken is destroyed in Rooms database.'
+        end
+        field :moodle_url do
+          label 'API URL'
+        end
+        field :moodle_token do
+          label 'API Token'
+        end
+        field :moodle_group_select_enabled do
+          label 'Group select enabled'
+        end
+        field :moodle_show_all_groups do
+          label 'Show all groups'
+        end
+      end
+
+      # Brightspace Configs
+      group :brightspace_configs do
+        label 'Brightspace configs'
+        active false
+
+        field :brightspace_integration_enabled do
+          label 'Brightspace integration enabled'
+          help 'If disabled, the Brightspace configs are not sent. The ConsumerConfigBrightspaceOauth is destroyed in Rooms database.'
+        end
+        field :brightspace_oauth_url do
+          label 'Brightspace URL'
+        end
+        field :brightspace_oauth_client_id do
+          label 'Client ID'
+        end
+        field :brightspace_oauth_client_secret do
+          label 'Client secret'
+        end
+        field :brightspace_oauth_scopes do
+          label 'OAuth scopes'
+        end
+      end
+    end
+  end
+  ### RoomsAppConfig ###
+
+
+  ### WorkaAppConfig ###
+  config.model WorkaAppConfig do
+    list do
+      configure [:created_at, :updated_at] do
+        hide
+      end
+    end
+
+    show do
+      configure [:created_at, :updated_at]
+    end
+
+    edit do
+      configure :tool do
+        hide
+      end
+    end
+  end
+  ### WorkaAppConfig ###
+
+
+  ### RsaKeyPair ###
+  config.model RsaKeyPair do
+    label 'RSA Key Pair'
+  end
+  ### RsaKeyPair ###
+
+
+  ### Tenant ###
+  config.model 'RailsLti2Provider::Tenant' do
+    object_label_method do
+      :uid
+    end
+    configure :uid do
+      label 'UID'
+    end
+
+    configure [:settings, :metadata] do
+      hide
+    end
+
+    list do
+      configure [:created_at, :updated_at] do
+        hide
+      end
+    end
+    create do
+      configure :tools do
+        hide
+      end
+    end
+    edit do
+      configure :tools do
+        hide
+      end
+    end
+  end
+  ### Tenant ###
+
+
+  ### Tool ###
+  config.model 'RailsLti2Provider::Tool' do
+    object_label_method do
+      :uuid
+    end
+
+    configure :uuid do
+      label 'UUID'
+    end
+
+    configure :app_settings do
+      hide
+    end
+
+    edit do
+      configure :lti_version, :enum do
+        # This block must return an array of strings
+        enum do
+          ['LTI-1p0', '1.3.0']
+        end
+        default_value 'LTI-1p0'
+      end
+
+      configure :tool_settings do
+        default_value 'none'
+        hide
+      end
+
+      configure [:lti_launches, :registrations] do
+        hide
+      end
+
+      configure :tenant do
+        inline_add false
+        inline_edit false
+      end
+
+      configure :shared_secret, :string
+    end
+
+    create do
+      configure :expired_at do
+        hide
+      end
+    end
+
+    list do
+      configure [:created_at, :updated_at, :tool_settings, :app_settings, :lti_launches, :registrations] do
+        hide
+      end
+    end
+
+    show do
+      configure [:tool_settings, :app_settings] do
+        hide
+      end
+      configure [:created_at, :updated_at]
+    end
+  end
+  ### Tool ###
 end
