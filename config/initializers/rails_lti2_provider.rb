31397a18ea08566b3ac56f4fa9d5f39fb7491821
Willian Pramio <willianpramio@hotmail.com>
2025-08-27 12:01:59 -0300

    feat(app_configs): add associations to Tools

--- config/initializers/rails_lti2_provider.rb
+++ config/initializers/rails_lti2_provider.rb
@@ -1,6 +1,9 @@
 Rails.application.config.to_prepare do
 
   RailsLti2Provider::Tenant.class_eval do
+    validates :uid, uniqueness: true
+    validates :institution_guid, uniqueness: true, allow_nil: true
+
     def set_app_settings(key_or_hash, value = nil, app_name:)
       if app_name.blank? || Doorkeeper::Application.find_by(name: app_name).nil?
         raise(ArgumentError, 'Invalid app name')
@@ -31,6 +34,17 @@ Rails.application.config.to_prepare do
   end
 
   RailsLti2Provider::Tool.class_eval do
+    validates :uuid, uniqueness: true
+
+    has_one :rooms_app_config, dependent: :destroy
+    has_one :worka_app_config, dependent: :destroy
+    has_one :bbb_config, dependent: :destroy
+    accepts_nested_attributes_for :rooms_app_config, :worka_app_config, :bbb_config
+
+    after_initialize do
+      self.tool_settings ||= 'none'
+    end
+
     def set_app_settings(key_or_hash, value = nil, app_name:)
       if app_name.blank? || Doorkeeper::Application.find_by(name: app_name).nil?
         raise(ArgumentError, 'Invalid app name')
